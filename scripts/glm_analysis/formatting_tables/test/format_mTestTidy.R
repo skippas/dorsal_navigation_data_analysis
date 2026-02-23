91f8a7cffe7ba44e96188a256b41c74b0db362b6
skippas <pav6330@gmail.com>
2026-02-23 13:26:31 +0100

    snapshot prior to refactoring tc tables formatting to correspond to data structures

--- scripts/glm_analysis/formatting_tables/test/format_mTestTidy.R
+++ scripts/glm_analysis/formatting_tables/test/format_mTestTidy.R
@@ -15,6 +15,19 @@ mTestTidyWideInLine <- mTestTidyInLine %>%
     names_glue = "{term_label}_{.value}"
   )
 
+mTestCoefsMsTab <- mTestTidy %>%
+  left_join(
+    unique(choices[, c("experiment", "nat_or_art")]),
+    by = "experiment"
+  ) %>%
+  transmute(
+    experiment, nat_or_art,
+    term = recode(term, !!!term_labels),
+    logOdds = sprintf("%.3f", estimate),
+    ci95 = sprintf("%.3f-%.3f", conf.low, conf.high),
+    pFmt = scales::pvalue(p.value, accuracy = 0.001)
+  )
+
 testTrialSlopeInLine <- mTestTidy %>%
   filter(term == "rank_trial") %>%
   left_join(
@@ -33,16 +46,3 @@ testTrialSlopeInLine <- mTestTidy %>%
 
 testTrialSlopeMsTab <- testTrialSlopeInLine %>%
   tibble::rownames_to_column("experiment")
-
-mTestCoefsMsTab <- mTestTidy %>%
-  left_join(
-    unique(choices[, c("experiment", "nat_or_art")]),
-    by = "experiment"
-  ) %>%
-  transmute(
-    experiment, nat_or_art,
-    term = recode(term, !!!term_labels),
-    logOdds = sprintf("%.3f", estimate),
-    ci95 = sprintf("%.3f-%.3f", conf.low, conf.high),
-    pFmt = scales::pvalue(p.value, accuracy = 0.001)
-  )
